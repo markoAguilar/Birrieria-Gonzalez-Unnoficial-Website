fb04a63c2d4387f239a1160e8d2db724e9848115
Marco Aguilar <marcoaguilar@Marcos-MBP-3.lan>
2024-04-15 07:38:52 -0700

    removed unecessary files

--- app.js
+++
@@ -1,7 +0,0 @@
-function initMap(){
-    const birrieria = {lat: 34.018, lng: -118.151};
-    const map = new google.maps.Map(document.getElementById("map"), {zoom:4, center: birrieria,});
-    const marker = new google.maps.marker({position: birrieria, map: map,});
-}
-
-window.initMap = initMap;
